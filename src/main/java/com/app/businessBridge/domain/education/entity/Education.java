e3111402141df1f7cce32088389642e3ba2c9f5f
DooHwanKim92 <rmfpsvldj01@naver.com>
2024-04-25 18:09:41 +0900

    add Education entity

---
+++ src/main/java/com/app/businessBridge/domain/education/entity/Education.java
@@ -0,0 +1,32 @@
+package com.app.businessBridge.domain.education.entity;
+
+
+import com.app.businessBridge.global.Jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Education extends BaseEntity {
+
+
+//    private Member author;
+
+    private String title;
+
+    private String content;
+
+    private MultipartFile videoFile;
+
+//    private MettingRoom mettingRoom;
+
+}
