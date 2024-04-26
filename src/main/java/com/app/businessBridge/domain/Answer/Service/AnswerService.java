3d745e17f6c2c4e6cdbca19d635a392bf28da160
류한선 <150202963+ryuhanseon@users.noreply.github.com>
2024-04-26 18:30:48 +0900

    Feature/1 (#23)

---
+++ src/main/java/com/app/businessBridge/domain/Answer/Service/AnswerService.java
@@ -0,0 +1,9 @@
+package com.app.businessBridge.domain.Answer.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+}
---
+++ src/main/java/com/app/businessBridge/domain/Club/Service/ClubService.java
@@ -0,0 +1,9 @@
+package com.app.businessBridge.domain.Club.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClubService {
+}
