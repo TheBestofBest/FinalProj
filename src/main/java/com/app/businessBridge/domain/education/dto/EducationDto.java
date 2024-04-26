632e5a7b1fd61dda02a08f311c39961cb4446b0d
DooHwanKim92 <rmfpsvldj01@naver.com>
2024-04-26 14:12:36 +0900

    add DTO each Entity, Next.js

---
+++ src/main/java/com/app/businessBridge/domain/education/dto/EducationDto.java
@@ -0,0 +1,10 @@
+package com.app.businessBridge.domain.education.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EducationDto {
+}
---
+++ src/main/java/com/app/businessBridge/domain/rebate/dto/RebateDto.java
@@ -0,0 +1,10 @@
+package com.app.businessBridge.domain.rebate.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RebateDto {
+}
