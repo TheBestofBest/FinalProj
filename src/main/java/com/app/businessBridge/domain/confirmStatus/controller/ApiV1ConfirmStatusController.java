1c0a05dc38b96b45d3ddaeb7e48d0ead0bd147d7
TwinOgre <glad2you@naver.com>
2024-04-26 17:25:35 +0900

    ConfirmStatusService_enhancement_feature/3

--- src/main/java/com/app/businessBridge/domain/confirmStatus/controller/ApiV1ConfirmStatusController.java
+++ src/main/java/com/app/businessBridge/domain/confirmStatus/controller/ApiV1ConfirmStatusController.java
@@ -9,9 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,12 +39,27 @@ public class ApiV1ConfirmStatusController {
 
 
     @Data
-    public static class createRequest {
+    public static class CreateConfirmStatusRequest {
         @NotBlank
         private String statusName;
         @NotBlank
         private String formDescription;
     }
+    @Getter
+    @AllArgsConstructor
+    public static class CreateConfirmStatusResponse {
+        private final ConfirmStatusDTO confirmStatusDTO;
+    }
+
+    @PostMapping("")
+    public RsData<CreateConfirmStatusResponse> createStatus(CreateConfirmStatusRequest createConfirmStatusRequest){
+        RsData<ConfirmStatus> confirmStatusRsData = this.confirmStatusService.create(createConfirmStatusRequest.statusName,createConfirmStatusRequest.formDescription);
+        return RsData.of(
+                confirmStatusRsData.getResultCode(),
+                confirmStatusRsData.getMsg(),
+                new CreateConfirmStatusResponse(new ConfirmStatusDTO(confirmStatusRsData.getData())));
+    }
+
 
 
 }
