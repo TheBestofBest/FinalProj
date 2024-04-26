f150957aa69afd4541eda2a5fa71504fe9c207b2
wintersky2 <emsw747@gmail.com>
2024-04-26 17:26:51 +0900

    PositionService CRUD done.

--- src/main/java/com/app/businessBridge/domain/department/entity/Department.java
+++ src/main/java/com/app/businessBridge/domain/department/entity/Department.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
+@Setter
 @SuperBuilder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/app/businessBridge/domain/position/service/PositionService.java
+++ src/main/java/com/app/businessBridge/domain/position/service/PositionService.java
@@ -6,11 +6,15 @@ import com.app.businessBridge.global.RsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PositionService {
     private final PostionRepository postionRepository;
 
+    // 직급 생성
     public RsData<Position> create(Integer positionCode, String positionName) {
         Position position = Position.builder()
                 .positionCode(positionCode)
@@ -18,6 +22,37 @@ public class PositionService {
                 .build();
         this.postionRepository.save(position);
 
-        return RsData.of("S-1", "success", position);
+        return RsData.of("S-02", "새로운 리소스가 성공적으로 생성되었습니다.", position);
+    }
+
+    // 직급 모두 불러오기
+    public RsData<List<Position>> read() {
+        List<Position> positionList = this.postionRepository.findAll();
+        return RsData.of("S-05", "요청한 리소스를 찾았습니다.", positionList);
+    }
+
+    // 직급 업데이트
+    public RsData<Position> update(Position position, Integer positionCode, String positionName) {
+        position.setPositionCode(positionCode);
+        position.setPositionName(positionName);
+        this.postionRepository.save(position);
+
+        return RsData.of("S-03", "리소스가 성공적으로 업데이트되었습니다.", position);
+    }
+
+    // 직급 삭제
+    public RsData delete(Long id) {
+        Optional<Position> op = findById(id);
+        if (op.isEmpty()) {
+            return RsData.of("F-04", "요청한 리소스를 찾을 수 없습니다.");
+        }
+
+        this.postionRepository.delete(op.get());
+        return RsData.of("S-04", "리소스가 성공적으로 삭제되었습니다.");
+    }
+
+    // 직급 단건 찾기 Optional
+    public Optional<Position> findById(Long id) {
+        return this.postionRepository.findById(id);
     }
 }
