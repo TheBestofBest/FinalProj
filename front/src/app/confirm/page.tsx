86986fb30b6471df3dfebc057908fb0ede34d4db
TwinOgre <glad2you@naver.com>
2024-05-01 18:41:15 +0900

    Confirm_page_featuring_feature/50

---
+++ front/src/app/confirm/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import ConfirmTable from "@/components/Confirm/ConfirmTable";
+
+export default function ConfirmPage() {
+  return (
+    <div className="h-full w-full overflow-scroll">
+      <ConfirmTable></ConfirmTable>
+    </div>
+  );
+}
