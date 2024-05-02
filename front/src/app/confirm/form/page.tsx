b44b647052c1b08d67e81d3f498fadc15f213720
TwinOgre <glad2you@naver.com>
2024-05-02 18:06:20 +0900

    ConfirmForm_page_feature/50

--- front/src/app/confirm/form/page.tsx
+++ front/src/app/confirm/form/page.tsx
@@ -6,6 +6,7 @@ export default function ConfirmForm() {
   return (
     <DefaultLayout>
       <div className="h-full w-full">
+        <h1>결재 등록</h1>
         <MarkdownPreviewer />
       </div>
     </DefaultLayout>
