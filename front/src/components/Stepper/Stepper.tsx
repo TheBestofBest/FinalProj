295c168c2a2e1dcc090a94311e3f1b6b9c9b3525
TwinOgre <145107336+TwinOgre@users.noreply.github.com>
2024-05-02 18:03:49 +0900

    PR# 55 : [전율택] [결재] 결재 메인 페이지 레이아웃 구현 Feature/50 (#55)
    * Confirm_form_page_featuring_markdown_feature/50

---
+++ front/src/components/Stepper/Stepper.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+
+const steps = ["반복문으로", "승인자 이름/직급", "Create an ad"];
+
+export default function HorizontalLinearAlternativeLabelStepper() {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={0} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
