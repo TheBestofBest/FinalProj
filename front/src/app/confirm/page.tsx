3b4d36a9a132f7ec4cfc3ae85b54c51726c98a77
TwinOgre <glad2you@naver.com>
2024-05-02 15:43:21 +0900

    Confirm_page_featuring2_feature/50

--- front/src/app/confirm/page.tsx
+++ front/src/app/confirm/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import ConfirmTable from "@/components/Confirm/ConfirmTable";
+import HorizontalLinearAlternativeLabelStepper from "@/components/Stepper/Stepper";
 
 export default function ConfirmPage() {
   return (
-    <div className="h-full w-full overflow-scroll">
-      <ConfirmTable></ConfirmTable>
-    </div>
+    <>
+      <div className="h-full w-full overflow-scroll">
+        <ConfirmTable></ConfirmTable>
+      </div>
+    </>
   );
 }
