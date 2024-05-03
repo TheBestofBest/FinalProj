b234cd7f223fc01d94a147fa2179ce63d02fe6b6
wintersky2 <emsw747@gmail.com>
2024-05-03 18:50:47 +0900

    api members/me 해결중

--- front/src/util/api.ts
+++ front/src/util/api.ts
@@ -1,8 +1,24 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
+import { Loader } from "next/dynamic";
 
 const api = axios.create({
   baseURL: "http://localhost:8090",
   withCredentials: true,
+});
+
+axios.interceptors.request.use(function () {
+  const queryClient = useQueryClient();
+  const getMember = async () => {
+    console.log("!!1141234313");
+    await api.get("/api/v1/members/me").then((res) => {
+      queryClient.setQueryData(["member"], res.data.data.memberDTO);
+    });
+  };
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ["member"],
+    queryFn: getMember,
+  });
 });
 
 export default api;
