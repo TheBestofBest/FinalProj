11296814062490bcb12ef9e964b0be1f64439e71
5binn <dhxnejqls13@naver.com>
2024-05-03 18:50:14 +0900

    temp

--- src/main/java/com/app/businessBridge/domain/relation/repository/MemberChatRepository.java
+++ src/main/java/com/app/businessBridge/domain/relation/repository/MemberChatRepository.java
@@ -13,6 +13,6 @@ public interface MemberChatRepository extends JpaRepository<MemberChatRelation, 
     @Query("SELECT mcr FROM MemberChatRelation mcr WHERE mcr.chattingRoom.id = :chattingRoomId")
     List<MemberChatRelation> findMemberChatRelationByChattingRoomId(@Param("chattingRoomId") Long id);
 
-    @Query("SELECT mcr FROM MemberChatRelation mcr WHERE mcr.member.username = :username")
-    List<MemberChatRelation> findMemberChatRelationByUsername(@Param("username") String username);
+    @Query("SELECT mcr FROM MemberChatRelation mcr WHERE mcr.member.id = :memberId")
+    List<MemberChatRelation> findMemberChatRelationByMemberId(@Param("memberId")  Long id);
 }
