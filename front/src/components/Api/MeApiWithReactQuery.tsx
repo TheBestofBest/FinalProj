5d2994a30785e4c988eb1ce40a2a1f239aefd8f0
wintersky2 <emsw747@gmail.com>
2024-05-08 14:45:05 +0900

    비회원시 강제 로그인페이지 이동, 회원시 로그인 페이지 접근불가, api.ts axios intersepter 사용철회, rebateDTO department,grade 간단 이름 수정.

--- front/src/components/Api/MeApiWithReactQuery.tsx
+++ front/src/components/Api/MeApiWithReactQuery.tsx
@@ -1,20 +1,24 @@
 import api from "@/util/api";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Loader from "../common/Loader";
+import { useRouter } from "next/navigation";
 
 export default function MeApiWithReactQuery() {
   const queryClient = useQueryClient();
+  const router = useRouter();
+
   const getMember = async () => {
-    console.log("!!1141234313");
     await api.get("/api/v1/members/me").then((res) => {
       queryClient.setQueryData(["member"], res.data.data.memberDTO);
     });
   };
+
   const { data, isLoading } = useQuery({
     queryKey: ["member"],
     queryFn: getMember,
   });
 
+  // 데이터 패칭 중
   if (isLoading) {
     return (
       <>
@@ -25,5 +29,10 @@ export default function MeApiWithReactQuery() {
 
   if (data) {
     return;
+  }
+
+  // 비 로그인 시 강제 이동
+  if (queryClient.getQueryData(["member"]) === undefined) {
+    router.replace("/auth/signin");
   }
 }
