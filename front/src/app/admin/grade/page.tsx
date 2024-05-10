4b14efd66ae47f04b65e301dc49faeedfd1d2bde
wintersky2 <emsw747@gmail.com>
2024-05-10 18:51:22 +0900

    부서 프론트중

--- front/src/app/admin/grade/page.tsx
+++ front/src/app/admin/grade/page.tsx
@@ -1,7 +1,26 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 const AdminGrade = () => {
-  return <DefaultLayout>직급</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <h2 className="text-title-md2 font-bold text-black dark:text-white">
+          직급 관리
+        </h2>
+
+        <nav>
+          <ol className="flex items-center gap-2">
+            <li>
+              <a className="font-medium" href="index.html">
+                관리 /
+              </a>
+            </li>
+            <li className="font-medium text-primary">직급</li>
+          </ol>
+        </nav>
+      </div>
+    </DefaultLayout>
+  );
 };
 
 export default AdminGrade;
