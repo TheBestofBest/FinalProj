e6956cae4ff0ae40ecd6c4df38cb7a6b8850be76
TwinOgre <glad2you@naver.com>
2024-05-10 16:20:44 +0900

    Confirm_form_type_enhancing1_feature/50

---
+++ front/src/types/ConfirmTypes/ConfirmTypes.ts
@@ -0,0 +1,11 @@
+export type confirmType = {
+subject: String
+description: String
+formData: String
+formType: ConfirmFormType
+confirmStatus: ConfirmStatus
+confirmRequestMember : Member
+confirmMembers: Member[]
+}
+
+export type: 
