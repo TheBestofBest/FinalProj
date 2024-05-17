40e4d12fc2ed1793afef6630a5da250c285f635f
wintersky <148525670+wintersky2@users.noreply.github.com>
2024-05-17 15:49:41 +0900

    init 데이터 문제 해결 핫픽스 (#115)

--- front/src/app/admin/account/page.tsx
+++ front/src/app/admin/account/page.tsx
@@ -17,6 +17,7 @@ import Image from "next/image";
 
 const AdminAccount = () => {
   const queryClient = useQueryClient();
+
   const getAllMember = async () => {
     const response = await api.get("/api/v1/members");
     console.log(response);
@@ -30,6 +31,12 @@ const AdminAccount = () => {
       .then((res) => {
         queryClient.setQueryData(["allMember"], res.data.data);
       });
+  };
+
+  const searchReset = async () => {
+    await api.get(`api/v1/members/search?keyword=`).then((res) => {
+      queryClient.setQueryData(["allMember"], res.data.data);
+    });
   };
 
   const { data } = useQuery({
@@ -56,7 +63,16 @@ const AdminAccount = () => {
       </div>
       <div className="flex justify-between">
         <AddAccountModal />
-        <form onSubmit={searchMembers}>
+        <form onSubmit={searchMembers} className="flex">
+          <button onClick={searchReset} className="mr-2 h-11 w-11">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 -960 960 960"
+              fill="#5f6368"
+            >
+              <path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
+            </svg>
+          </button>
           <input
             type="text"
             name="keyword"
