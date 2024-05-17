e02968e71e8c4b9495f50f71b460bcb0311e5c2f
5binn <dhxnejqls13@naver.com>
2024-05-17 16:12:20 +0900

    마무리

--- front/src/app/Message.tsx
+++ front/src/app/Message.tsx
@@ -1,113 +1,79 @@
+'use client'
+
 import Link from "next/link";
 import Image from "next/image";
 import { Chat } from "@/types/chat";
+import { useRouter } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { ChattingRoom } from "./chatting/type";
+import api from "@/util/api";
 
-const chatData: Chat[] = [
-  {
-    avatar: "/images/user/user-01.png",
-    name: "스티븐 잡스",
-    text: "우리 회사 오실?",
-    time: 12,
-    textCount: 3,
-    dot: 3,
-  },
-  {
-    avatar: "/images/user/user-02.png",
-    name: "야쿠르트 아줌마",
-    text: "야쿠르트 사세요!",
-    time: 12,
-    textCount: 0,
-    dot: 1,
-  },
-  {
-    avatar: "/images/user/user-04.png",
-    name: "모르는 사람",
-    text: "안녕하세요 고갱님?",
-    time: 32,
-    textCount: 0,
-    dot: 3,
-  },
-  {
-    avatar: "/images/user/user-05.png",
-    name: "부장님",
-    text: "김 군 이 업무좀",
-    time: 32,
-    textCount: 2,
-    dot: 6,
-  },
-  {
-    avatar: "/images/user/user-01.png",
-    name: "빌 게이츠",
-    text: "잡스 연락옴?",
-    time: 32,
-    textCount: 0,
-    dot: 3,
-  },
-  {
-    avatar: "/images/user/user-03.png",
-    name: "선배님",
-    text: "그따구로 할래?",
-    time: 32,
-    textCount: 3,
-    dot: 6,
-  },
-];
 
 const Message = () => {
+
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const memberData: any = queryClient.getQueryData(["member"]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [chattingRooms, setChattingRooms] = useState<ChattingRoom[]>([]);
+  const [isEmpty, setIsEmpty] = useState<boolean>();
+
+  useEffect(() => {
+    fetchChattingRooms();
+  }, []);
+
+  const fetchChattingRooms = () => {
+    api.get('/api/v1/chats')
+      .then(response => {
+        setChattingRooms(response.data.data.chattingRoomDtoList);
+        setIsEmpty(false);
+      }).catch(err => {
+        setIsEmpty(true);
+      })
+  }
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
-      <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
-        쪽지함
+      <h4 className="px-7.5 text-xl font-semibold border-[#eee] text-black dark:text-white border-b pb-4">
+        채팅방
       </h4>
-
-      <div>
-        {chatData.map((chat, key) => (
-          <Link
-            href="/"
-            className="flex items-center gap-5 px-7.5 py-3 hover:bg-gray-3 dark:hover:bg-meta-4"
-            key={key}
-          >
-            <div className="relative h-14 w-14 rounded-full">
-              <Image
-                width={56}
-                height={56}
-                src={chat.avatar}
-                alt="User"
-                style={{
-                  width: "auto",
-                  height: "auto",
-                }}
-              />
-              <span
-                className={`absolute bottom-0 right-0 h-3.5 w-3.5 rounded-full border-2 border-white ${
-                  chat.dot === 6 ? "bg-meta-6" : `bg-meta-${chat.dot}`
-                } `}
-              ></span>
-            </div>
-
-            <div className="flex flex-1 items-center justify-between">
-              <div>
-                <h5 className="font-medium text-black dark:text-white">
-                  {chat.name}
-                </h5>
-                <p>
-                  <span className="text-sm text-black dark:text-white">
-                    {chat.text}
-                  </span>
-                  <span className="text-xs"> . {chat.time} min</span>
-                </p>
+      <div className="p-2 h-125 overflow-y-auto">
+        {isEmpty ? <></> :
+          <>
+            {chattingRooms.map((chattingRoom: ChattingRoom) => (
+              <Link
+                href={"/chatting/" + chattingRoom.id}
+                className="border-b border-[#eee] flex items-center gap-5 px-7.5 py-3 hover:bg-gray-3 dark:hover:bg-meta-4"
+                key={chattingRoom.id}
+              >
+                <div className="px-1 flex flex-1 items-center justify-between">
+                  <div>
+                    <h5 className="font-medium text-black dark:text-white">
+                      {chattingRoom.name}
+                    </h5>
+                    <p>
+                      <span className="text-sm">{chattingRoom?.members.map((name, index) =>
+                        <React.Fragment key={index}>
+                          {index == 0 ? name : `, ${name}`}
+                        </React.Fragment>)}
+                      </span>
+                      {/* <span className="text-xs"> . {chat.time} min</span> */}
+                    </p>
+                  </div>
+                  {/* {chat.textCount !== 0 && (
+              <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
+                <span className="text-sm font-medium text-white">
+                  {" "}
+                  {chat.textCount}
+                </span>
               </div>
-              {chat.textCount !== 0 && (
-                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
-                  <span className="text-sm font-medium text-white">
-                    {" "}
-                    {chat.textCount}
-                  </span>
+            )} */}
                 </div>
-              )}
-            </div>
-          </Link>
-        ))}
+              </Link>
+            ))}
+          </>
+        }
+
       </div>
     </div>
   );
