148dc22c58fd0d05e1c36329481cd3daa0d3fad4
YulTaek Jeon <145107336+TwinOgre@users.noreply.github.com>
2024-05-17 15:04:26 +0900

    PR#109 - [전율택] 결재 삭제, 승인, 반려 기능 추가 (#109)
    * confirm_controller_formData_fixing_feature/96

--- src/main/java/com/app/businessBridge/domain/confirmStatus/repository/ConfirmStatusRepository.java
+++ src/main/java/com/app/businessBridge/domain/confirmStatus/repository/ConfirmStatusRepository.java
@@ -4,6 +4,9 @@ import com.app.businessBridge.domain.confirmStatus.entity.ConfirmStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfirmStatusRepository extends JpaRepository<ConfirmStatus, Long> {
+    Optional<ConfirmStatus> findByStatusName(String statusName);
 }
